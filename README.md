08e1ee1bc2284408748214eabdcdbe816bbf84d1
Bartek Cib <bciborowski.pl@gmail.com>
2020-03-22 15:00:54 +0100

    Some foundation added README.md added

---
+++ README.md
@@ -0,0 +1,22 @@
+#Training Management System - System Zarządzania Szkoleniami
+
+##Krótki opis systemu
+W ramach projektu należy utworzyć system usprawniający zarządzanie prowadzeniem szkoleń.
+
+###Główne funkcje systemu
+* Rejestracja administratora, prowadzących oraz kursantów
+* Kalendarz zajęć
+* Udostępnianie przez prowadzących materiałów dotyczących zajęć
+* Podsystem powiadomień
+###Technologie
+* Spring + Hibernate
+* (opcjonalnie) Frontend w Angular
+
+###Podstawowe byty (propozycja)
+* Użytkownik
+* Zgłoszenie uczestnika
+* Kurs
+* Blok zajęć
+* Zajęcia
+* Powiadomienie
+* Powiadomienia użytkownika
---
+++ src/main/java/pl/bciborowski/trainingmanagement/controllers/IndexController.java
@@ -0,0 +1,24 @@
+package pl.bciborowski.trainingmanagement.controllers;
+
+import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    private String indexMode;
+
+    @Autowired
+    public IndexController(Environment environment) {
+        indexMode = environment.getProperty("index-mode");
+    }
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("mode", indexMode);
+        return "index";
+    }
+}
