22ee075a0b67ba196dc19acb3c4988db3b31e9e7
Bartek Cib <bciborowski.pl@gmail.com>
2020-03-22 14:12:41 +0100

    Initial commit

---
+++ src/main/java/pl/bciborowski/trainingmanagement/TrainingManagementApplication.java
@@ -0,0 +1,13 @@
+package pl.bciborowski.trainingmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/bciborowski/trainingmanagement/TrainingManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bciborowski.trainingmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
